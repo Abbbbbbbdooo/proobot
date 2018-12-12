8e46824f922e8f6228c7d5ae15325b36a1780156
Abdo <45042294+Abbbbbbbdooo@users.noreply.github.com>
2018-12-12 14:31:10 +0200

    Update probot.js

--- probot.js
+++ probot.js
@@ -3,8 +3,8 @@ const moment = require("moment");
 const fs = require("fs");      
 const dateFormat = require('dateformat');
 const client = new Discord.Client(); 
-const Canvas = require("canvas"); //npm i canvas
-const prefix = "YOUR Prefix Here" // برفكس
+const Canvas = require("canvas");
+const prefix = "#"
 let profile = JSON.parse(fs.readFileSync("profile.json", "utf8"))
 
 
@@ -824,4 +824,4 @@ message.channel.stopTyping()
 })
 }
 });
-client.login("token Your Bot Here");  // توكن حقق
+client.login(process.env.BOT_TOKEN)
